893c4f6a72f2f3f21ef49d8aed764358a211f54f
Raphael Tahar <raphael.tahar@ledger.fr>
2025-04-03 09:38:43 +0200

    feat: first bootstrap commit

---
+++ libs/shared/ui/.storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { mergeConfig } from 'vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  core: {
+    builder: '@storybook/builder-vite'
+  },
+  viteFinal: async (config: ViteConfig) => {
+    return mergeConfig(config, {
+      plugins: [nxViteTsPaths()],
+    });
+  }
+};
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
