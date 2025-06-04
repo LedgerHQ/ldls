f29ad63bd6227f58028f97a080d27a7836445c64
Bogdan Brezuica <bogdan.brezuica-ext@ledger.fr>
2025-06-04 12:39:01 +0300

    fix

--- libs/design-tokens/src/themes.ts
+++ libs/design-tokens/src/themes.ts
@@ -2,8 +2,8 @@ import { merge } from 'lodash';
 import { tokens as enterpriseLightThemeTokens } from './lib/enterprise/theme.light.js';
 import { tokens as enterpriseDarkThemeTokens } from './lib/enterprise/theme.dark.js';
 
-import { tokens as ledgerLiveLightThemeTokens } from './lib/ledgerlive/theme.light.js';
-import { tokens as ledgerLiveDarkThemeTokens } from './lib/ledgerlive/theme.dark.js';
+import { tokens as ledgerLiveLightThemeTokens } from './lib/ledger-live/theme.light.js';
+import { tokens as ledgerLiveDarkThemeTokens } from './lib/ledger-live/theme.dark.js';
 
 import { tokens as websitesLightThemeTokens } from './lib/websites/theme.light.js';
 import { tokens as websitesDarkThemeTokens } from './lib/websites/theme.dark.js';
