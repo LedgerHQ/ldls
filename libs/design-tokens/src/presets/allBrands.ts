0d13497f6667e2f1c2578c4bd7423c074a3b74f9
aammami-ledger <abla.ammami@gmail.com>
2025-06-25 19:55:47 +0200

    fix: update import path for allBrandsTheme in allBrands preset

--- libs/design-tokens/src/presets/allBrands.ts
+++ libs/design-tokens/src/presets/allBrands.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 import { createPrimitivesPlugin } from '../utils/create-primitives-plugin.js';
 import { createThemePlugin } from '../utils/index.js';
-import { allBrandsTheme } from 'src/themes/index.js';
+import { allBrandsTheme } from '../themes/index.js';
 
 export const allBrandsPreset = {
   content: [],
