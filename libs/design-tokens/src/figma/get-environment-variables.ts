43902888f8e43db2a75315a35c2899e867049f11
Bogdan Brezuica <bogdan.brezuica-ext@ledger.fr>
2025-07-02 16:45:12 +0300

    fix: remove usage of node-fetch

--- libs/design-tokens/src/figma/get-environment-variables.ts
+++ libs/design-tokens/src/figma/get-environment-variables.ts
@@ -1,13 +1,15 @@
-export default function getEnvironmentVariables(): { figmaToken: string; fileKey: string } {
+export default function getEnvironmentVariables(): {
+  figmaToken: string;
+  fileKey: string;
+} {
   const figmaToken = process.env.FIGMA_API_TOKEN;
-  const fileKey = process.env.FIGMA_FILE_KEY;
+  const fileKey = process.env.FIGMA_FOUNDATIONS_FILE_KEY;
 
   if (!figmaToken || !fileKey) {
     throw new Error(
-      `Please set the FIGMA_API_TOKEN and FIGMA_FILE_KEY environment variables.`
+      `Please set the FIGMA_API_TOKEN and FIGMA_FOUNDATIONS_FILE_KEY environment variables.`,
     );
   }
 
   return { figmaToken, fileKey };
 }
-
