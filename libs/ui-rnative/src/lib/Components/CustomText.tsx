077359be4f0e6d1729a43a4ef4470b91529a2e65
Bogdan Brezuica <bogdan.brezuica-ext@ledger.fr>
2025-07-31 16:40:28 +0300

    cleanup

--- libs/ui-rnative/src/lib/Components/CustomText.tsx
+++
@@ -1,6 +0,0 @@
-import { Text, TextProps } from 'react-native';
-
-export const CustomText = ({ children, ...props }: TextProps) => {
-  return <Text {...props}>{children}</Text>;
-};
-CustomText.displayName = 'CustomText';
