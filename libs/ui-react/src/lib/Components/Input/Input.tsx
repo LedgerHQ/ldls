7f504610d7c095e4e8911cfcf770912bb4fe561e
aammami-ledger <abla.ammami@ledger.fr>
2025-09-04 08:48:20 +0100

    feat: move BassInput to it's own folder and do some minor changes

--- libs/ui-react/src/lib/Components/Input/Input.tsx
+++ libs/ui-react/src/lib/Components/Input/Input.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BaseInput, type BaseInputProps } from './BaseInput';
+import { BaseInput, type BaseInputProps } from '../BaseInput';
 
 export type InputProps = Omit<BaseInputProps, 'prefix'>;
 
