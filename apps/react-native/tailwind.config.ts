4c1457c0e5250884c524db7fcd023282d16ecd01
Simon Bruneaud <simon.bruneaud@ledger.fr>
2025-09-25 17:07:01 +0200

    chore: few RN setup improvements
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- apps/react-native/tailwind.config.ts
+++ apps/react-native/tailwind.config.ts
@@ -4,10 +4,7 @@ import { ledgerLivePreset } from '@ledgerhq/ldls-design-core';
 const config = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    './.storybook/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.stories.{js,jsx,ts,tsx}',
-    '../../libs/ui-rnative/dist/**/*.{js,jsx,ts,tsx}',
-    '../../libs/ui-rnative/src/**/*.{js,jsx,ts,tsx}',
+    '../../node_modules/@ledgerhq/ldls-ui-rnative/dist/**/*.{js,jsx,ts,tsx}',
   ],
   presets: [require('nativewind/preset'), ledgerLivePreset],
 } satisfies Config;
