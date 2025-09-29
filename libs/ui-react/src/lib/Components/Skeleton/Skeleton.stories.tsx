7f7fbfb0233245b182bec5dcce30001077910f4e
aammami-ledger <abla.ammami@ledger.fr>
2025-09-29 18:06:50 +0200

    fix: update Skeleton component title from 'Components/Skeleton' to 'Communication/Skeleton'

--- libs/ui-react/src/lib/Components/Skeleton/Skeleton.stories.tsx
+++ libs/ui-react/src/lib/Components/Skeleton/Skeleton.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Skeleton } from './Skeleton';
 
 const meta: Meta<typeof Skeleton> = {
-  title: 'Components/Skeleton',
+  title: 'Communication/Skeleton',
   component: Skeleton,
   parameters: {
     layout: 'centered',
