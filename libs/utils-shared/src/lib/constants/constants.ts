18722eecc88fa5f07f330ea9dea941f9cf4d9589
Simon Bruneaud <simon.bruneaud@ledger.fr>
2025-10-02 15:52:16 +0200

    fix: remove the use of process, doesnt work on storybook for now
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- libs/utils-shared/src/lib/constants/constants.ts
+++ libs/utils-shared/src/lib/constants/constants.ts
@@ -8,15 +8,3 @@ export const isClient: boolean = isWeb && isWindowDefined;
 export const useIsomorphicLayoutEffect: typeof useEffect = isServer
   ? useEffect
   : useLayoutEffect;
-
-export const isChrome: boolean =
-  typeof navigator !== 'undefined' && /Chrome/.test(navigator.userAgent || '');
-
-export const isWebTouchable: boolean =
-  isClient && ('ontouchstart' in window || navigator.maxTouchPoints > 0);
-
-export const isTouchable: boolean = !isWeb || isWebTouchable;
-// set :boolean to avoid inferring type to false
-export const isAndroid = false;
-export const isIos: boolean = process.env.TEST_NATIVE_PLATFORM === 'ios';
-export const currentPlatform: 'web' | 'ios' | 'native' | 'android' = 'web';
