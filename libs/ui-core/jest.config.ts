5e3e2e5d23ad5f49e770f61b98d4542293d74024
Simon.b <15988848+gamegee@users.noreply.github.com>
2025-10-07 18:31:20 +0200

    chore/make utils shared publishable lib 2 (#247)
    * chore: completely remove @ldls for libs
    
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>
    
    * chore: nx release plan
    
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>
    
    * docs: update docs
    
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>
    
    ---------
    
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- libs/ui-core/jest.config.ts
+++ libs/ui-core/jest.config.ts
@@ -9,7 +9,7 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@ldls/ui-core',
+  displayName: '@ledgerhq/ldls-ui-core',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
