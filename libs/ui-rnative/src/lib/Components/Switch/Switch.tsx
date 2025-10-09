2cb6be59822725def80786152ee21432cf106fa8
Simon.b <15988848+gamegee@users.noreply.github.com>
2025-10-09 16:41:31 +0200

    Update libs/ui-rnative/src/lib/Components/Switch/Switch.tsx
    Co-authored-by: Abla Ammami <abla.ammami@ledger.fr>

--- libs/ui-rnative/src/lib/Components/Switch/Switch.tsx
+++ libs/ui-rnative/src/lib/Components/Switch/Switch.tsx
@@ -7,7 +7,7 @@ import { useControllableState } from '../../utils';
 
 /**
  * The switch follows the design system tokens and supports checked, unchecked,
- * disabled, and focus states with proper hover and active interactions.
+ * disabled, and focus states with proper active interactions.
  *
  * @see {@link https://ldls.vercel.app/?path=/docs/react-native_selection-switch--docs Storybook}
  * @see {@link https://ldls.vercel.app/?path=/docs/react-native_selection-switch--docs#dos-and-donts Guidelines}
