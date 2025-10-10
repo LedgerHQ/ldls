58814e94e102a6b4f8bfc2b7b850e6f1a67d1731
Simon Bruneaud <simon.bruneaud@ledger.fr>
2025-10-10 11:56:08 +0200

    feat(rnative): add Wrap and Banner components
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

---
+++ libs/ui-rnative/src/lib/Components/Banner/types.ts
@@ -0,0 +1,33 @@
+import { ReactNode } from 'react';
+import { ViewProps } from 'react-native';
+
+export type BannerProps = ViewProps & {
+  /**
+   * The type of banner which affects color and icon.
+   */
+  appearance?: 'info' | 'success' | 'warning' | 'error';
+  /**
+   * The main title of the banner.
+   */
+  title: string;
+  /**
+   * Optional descriptive text.
+   */
+  description?: ReactNode;
+  /**
+   * Optional primary action.
+   */
+  primaryAction?: React.ReactNode;
+  /**
+   * Optional secondary action.
+   */
+  secondaryAction?: React.ReactNode;
+  /**
+   * Optional close action.
+   */
+  onClose?: () => void;
+  /**
+   * Optional aria label for the close button.
+   */
+  closeAriaLabel?: string;
+};
