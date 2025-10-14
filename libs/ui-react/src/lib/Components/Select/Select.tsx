b8a5ad720b6d6248104d45305668dbeac02e6bb6
aammami-ledger <abla.ammami@ledger.fr>
2025-10-14 11:33:11 +0200

    style(Select): update scroll up and down button styles to include text-muted class

--- libs/ui-react/src/lib/Components/Select/Select.tsx
+++ libs/ui-react/src/lib/Components/Select/Select.tsx
@@ -176,7 +176,7 @@ function SelectScrollUpButton({
     <SelectPrimitive.ScrollUpButton
       data-slot='select-scroll-up-button'
       className={cn(
-        'flex cursor-default items-center justify-center py-1',
+        'flex cursor-default items-center justify-center py-1 text-muted',
         className,
       )}
       {...props}
@@ -194,7 +194,7 @@ function SelectScrollDownButton({
     <SelectPrimitive.ScrollDownButton
       data-slot='select-scroll-down-button'
       className={cn(
-        'flex cursor-default items-center justify-center py-1',
+        'flex cursor-default items-center justify-center py-1 text-muted',
         className,
       )}
       {...props}
