60e1be89509f6c06f9a256c34a839f1c5a67fae2
Simon Bruneaud <simon.bruneaud@ledger.fr>
2025-10-20 17:28:10 +0200

    refactor(app-sandbox): setup dark mode + use ESM + remove extra-useless modules
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- apps/app-sandbox-react/src/app/app.tsx
+++ apps/app-sandbox-react/src/app/app.tsx
@@ -1,48 +1,26 @@
-import { Route, Routes, Link } from 'react-router-dom';
-import NxWelcome from './nx-welcome';
+import {
+  Button,
+  ThemeProvider,
+  Switch,
+  useThemeState,
+} from '@ledgerhq/ldls-ui-react';
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title='ldls' />
+  const { theme, toggleTheme } = useThemeState();
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role='navigation'>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/page-2'>Page 2</Link>
-          </li>
-        </ul>
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='bg-muted flex h-screen w-screen flex-col items-center justify-center'>
+        <div className='text-muted flex flex-row gap-8'>
+          Dark mode
+          <Switch selected={theme === 'dark'} onChange={toggleTheme} />
+        </div>
+        <div className='mt-32 flex flex-row gap-2'>
+          <Button appearance='accent'>Button</Button>
+          <Button appearance='base'>Button</Button>
+        </div>
       </div>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to='/page-2'>Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path='/page-2'
-          element={
-            <div>
-              <Link to='/'>Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ apps/app-sandbox-react/tailwind.config.ts
@@ -0,0 +1,11 @@
+import { Config } from 'tailwindcss';
+import { ledgerLivePreset } from '@ledgerhq/ldls-design-core';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{ts,tsx,html}',
+    '../../node_modules/@ledgerhq/ldls-ui-react/dist/**/**/*.{ts,js,tsx,jsx}',
+  ],
+  presets: [ledgerLivePreset],
+} satisfies Config;
