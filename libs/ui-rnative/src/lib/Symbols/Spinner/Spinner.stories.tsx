4f14a84d3095b6a813cc715133b7f85c547523b2
Miguel Mano <aquelemiguel@gmail.com>
2025-10-28 15:16:14 +0000

    update docs for rn

--- libs/ui-rnative/src/lib/Symbols/Spinner/Spinner.stories.tsx
+++ libs/ui-rnative/src/lib/Symbols/Spinner/Spinner.stories.tsx
@@ -1,28 +1,101 @@
-import { Meta } from '@storybook/react-vite';
+import { Meta, StoryObj } from '@storybook/react-native-web-vite';
 import { Spinner } from './Spinner';
 
 const meta: Meta<typeof Spinner> = {
   title: 'Symbols/Spinner',
   component: Spinner,
+  parameters: {
+    actions: { disable: true },
+  },
   argTypes: {
     size: {
       control: 'select',
-      options: [16, 20, 24],
+      options: [12, 16, 20, 24, 40, 48, 56],
+    },
+    appearance: {
+      control: 'select',
+      options: ['base', 'accent'],
     },
   },
 };
 
 export default meta;
+type Story = StoryObj<typeof Spinner>;
 
-export const Basic = () => (
-  <div className='flex items-center justify-center'>
-    <Spinner size={16} />
-  </div>
-);
+export const Basic: Story = {
+  args: {
+    appearance: 'base',
+    size: 40,
+  },
+  render: ({ size, appearance }) => (
+    <div className='flex items-center justify-center'>
+      <Spinner size={size} appearance={appearance} />
+    </div>
+  ),
+};
 
-export const AppearancesShowcase = () => (
-  <div className='flex items-center justify-center gap-4'>
-    <Spinner size={16} appearance='base' />
-    <Spinner size={16} appearance='accent' />
-  </div>
-);
+export const AppearanceShowcase: Story = {
+  args: {
+    size: 40,
+  },
+  argTypes: {
+    appearance: {
+      control: false,
+    },
+  },
+  render: ({ size }) => (
+    <div className='flex items-center justify-center gap-32'>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={size} appearance='base' />
+        <span className='text-muted body-4'>Base</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={size} appearance='accent' />
+        <span className='text-muted body-4'>Accent</span>
+      </div>
+    </div>
+  ),
+};
+
+export const SizeShowcase: Story = {
+  args: {
+    appearance: 'base',
+  },
+  argTypes: {
+    size: {
+      control: false,
+    },
+  },
+  render: ({ appearance }) => (
+    <div className='flex items-center justify-center gap-32'>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={12} appearance={appearance} />
+        <span className='text-muted body-4'>12</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={16} appearance={appearance} />
+        <span className='text-muted body-4'>16</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={20} appearance={appearance} />
+        <span className='text-muted body-4'>20</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={24} appearance={appearance} />
+        <span className='text-muted body-4'>24</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={40} appearance={appearance} />
+        <span className='text-muted body-4'>40</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={48} appearance={appearance} />
+        <span className='text-muted body-4'>48</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={56} appearance={appearance} />
+        <span className='text-muted body-4'>56</span>
+      </div>
+    </div>
+  ),
+};
--- libs/ui-rnative/src/lib/Symbols/Spinner/Spinner.tsx
+++ libs/ui-rnative/src/lib/Symbols/Spinner/Spinner.tsx
@@ -1,25 +1,46 @@
 import { cn } from '@ledgerhq/ldls-utils-shared';
+import { cva } from 'class-variance-authority';
 import { forwardRef } from 'react';
 import Svg from 'react-native-svg';
+import { IconSize } from '../../Components/Icon/Icon.types';
 import { Spinner as SpinnerIcon } from '../Icons/Spinner';
 
-export type SpinnerProps = React.SVGProps<SVGSVGElement> & {
-  size?: 16 | 20 | 24;
-  appearance?: 'base' | 'accent'; // TODO: use cva later
+export type SpinnerProps = {
+  size?: IconSize;
+  appearance?: 'base' | 'accent';
+  className?: string;
 };
+
+const iconVariants = cva('shrink-0 animate-spin', {
+  variants: {
+    appearance: {
+      base: 'text-base',
+      accent: 'text-interactive',
+    },
+  },
+  defaultVariants: {
+    appearance: 'base',
+  },
+});
 
 /**
  * A basic spinner component for loading states.
  *
  * @example
  * <Spinner />
+ *
+ * @default size 16
+ * @default appearance 'base'
  */
-export const Spinner = forwardRef<Svg, SpinnerProps>(({ className }, ref) => {
-  return (
-    <SpinnerIcon
-      ref={ref}
-      className={cn('shrink-0 animate-spin', className)}
-      accessibilityLabel='Loading'
-    />
-  );
-});
+export const Spinner = forwardRef<Svg, SpinnerProps>(
+  ({ className, size = 16, appearance = 'base' }, ref) => {
+    return (
+      <SpinnerIcon
+        size={size}
+        className={cn(iconVariants({ appearance }), className)}
+        ref={ref}
+        accessibilityLabel='Loading'
+      />
+    );
+  },
+);
