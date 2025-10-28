cb85dcbdb3418c6df286a8db656556a264aef839
Miguel Mano <aquelemiguel@gmail.com>
2025-10-28 12:19:00 +0000

    use cva

--- libs/ui-react/src/lib/Symbols/Spinner/Spinner.tsx
+++ libs/ui-react/src/lib/Symbols/Spinner/Spinner.tsx
@@ -1,32 +1,41 @@
 import { cn } from '@ledgerhq/ldls-utils-shared';
+import { cva } from 'class-variance-authority';
 import { forwardRef } from 'react';
 import { IconSize } from '../../Components/Icon/Icon';
 import { Spinner as SpinnerIcon } from '../Icons/Spinner';
 
 export type SpinnerProps = React.SVGProps<SVGSVGElement> & {
   size?: IconSize;
-  appearance?: 'base' | 'accent'; // TODO: use cva later
+  appearance?: 'base' | 'accent';
 };
+
+const iconVariants = cva('shrink-0 animate-spin', {
+  variants: {
+    appearance: {
+      base: 'text-on-accent',
+      accent: 'text-on-interactive',
+    },
+  },
+  defaultVariants: {
+    appearance: 'base',
+  },
+});
 
 /**
  * A basic spinner component for loading states.
  *
  * @example
  * <Spinner />
+ *
+ * @default size 16
+ * @default appearance 'base'
  */
 export const Spinner = forwardRef<SVGSVGElement, SpinnerProps>(
   ({ className, size = 16, appearance = 'base', ...props }, ref) => {
     return (
       <SpinnerIcon
         size={size}
-        className={cn(
-          'shrink-0 animate-spin',
-          {
-            'text-on-accent': appearance === 'base',
-            'text-on-interactive': appearance === 'accent',
-          },
-          className,
-        )}
+        className={cn(iconVariants({ appearance }), className)}
         ref={ref}
         aria-label='Loading'
         {...props}
