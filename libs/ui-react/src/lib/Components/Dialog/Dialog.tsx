80d99a901de24131daecb3a1da8368c44d8aa74e
Abla Ammami <abla.ammami@ledger.fr>
2025-10-28 09:50:21 +0100

    feat(design-core): Add animations to our foundation documentation  (#275)
    * feat(animations): introduce animation system with utility classes and documentation
    
    * fix: revert unnecessary change
    
    * chore: fix `translate-from-right` animation
    
    * feat(animations): add content show and hide animations and small QA changes
    
    * feat(dialog): restore show and hide animations to dialog content
    
    * refactor(animations): layout updates + add reset button
    
    * refactor(docs): update table styling in documentation for consistency
    
    * chore(animations): remove unused CSS and TypeScript animation files
    
    * refactor(animations): enhance animation story layout and replace code snippets with Tag components
    
    * feat(animations): add delay demo class and update animation example in documentation

--- libs/ui-react/src/lib/Components/Dialog/Dialog.tsx
+++ libs/ui-react/src/lib/Components/Dialog/Dialog.tsx
@@ -170,7 +170,7 @@ export function DialogContent({
       <DialogPrimitive.Content
         data-slot='dialog-content'
         className={cn(
-          'fixed left-[50%] top-[50%] z-dialog-content w-[400px] max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] overflow-hidden rounded-2xl bg-canvas-sheet px-24 pb-24 pt-0 data-[state=closed]:animate-content-hide data-[state=open]:animate-content-show',
+          'fixed left-[50%] top-[50%] z-dialog-content w-400 max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] overflow-hidden rounded-2xl bg-canvas-sheet px-24 pb-24 pt-0 data-[state=closed]:animate-content-hide data-[state=open]:animate-content-show',
           className,
         )}
         {...props}
