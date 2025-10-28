1fb526e6c783ae19d4818fcb974a50d2d28ef4fa
Miguel Mano <aquelemiguel@gmail.com>
2025-10-28 12:49:04 +0000

    use more idiomatic story creation

--- libs/ui-react/src/lib/Symbols/Spinner/Spinner.stories.tsx
+++ libs/ui-react/src/lib/Symbols/Spinner/Spinner.stories.tsx
@@ -1,49 +1,85 @@
-import { Meta } from '@storybook/react-vite';
-import { Spinner, SpinnerProps } from './Spinner';
+import { Meta, StoryObj } from '@storybook/react-vite';
+import { Spinner } from './Spinner';
 
 const meta: Meta<typeof Spinner> = {
   title: 'Symbols/Spinner',
   component: Spinner,
+  parameters: {
+    actions: { disable: true },
+  },
   argTypes: {
     size: {
       control: 'select',
-      options: [16, 20, 24],
+      options: [12, 16, 20, 24, 40, 48, 56],
+    },
+    appearance: {
+      control: 'select',
+      options: ['base', 'accent'],
     },
   },
 };
 
 export default meta;
+type Story = StoryObj<typeof Spinner>;
 
-export const Basic = () => (
-  <div className='flex items-center justify-center'>
-    <Spinner size={40} />
-  </div>
-);
-
-export const Appearances = () => {
-  const appearances: SpinnerProps['appearance'][] = ['base', 'accent'];
-  return (
-    <div className='flex items-center justify-center gap-32'>
-      {appearances.map((appearance) => (
-        <div className='flex flex-col items-center gap-4' key={appearance}>
-          <Spinner size={40} appearance={appearance} />
-          <span className='text-muted body-4'>{appearance}</span>
-        </div>
-      ))}
+export const Basic: Story = {
+  args: {
+    appearance: 'base',
+    size: 24,
+  },
+  render: ({ size, appearance }) => (
+    <div className='flex items-center justify-center'>
+      <Spinner size={size} appearance={appearance} />
     </div>
-  );
+  ),
 };
 
-export const Sizes = () => {
-  const sizes: SpinnerProps['size'][] = [12, 16, 20, 24, 40, 48, 56];
-  return (
+export const Appearances: Story = {
+  render: () => (
     <div className='flex items-center justify-center gap-32'>
-      {sizes.map((size) => (
-        <div className='flex flex-col items-center gap-4'>
-          <Spinner size={size} />
-          <span className='text-muted body-4'>{size}</span>
-        </div>
-      ))}
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={40} appearance='base' />
+        <span className='text-muted body-4'>Base</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={40} appearance='accent' />
+        <span className='text-muted body-4'>Accent</span>
+      </div>
     </div>
-  );
+  ),
+};
+
+export const Sizes: Story = {
+  render: () => (
+    <div className='flex items-center justify-center gap-32'>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={12} />
+        <span className='text-muted body-4'>12</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={16} />
+        <span className='text-muted body-4'>16</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={20} />
+        <span className='text-muted body-4'>20</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={24} />
+        <span className='text-muted body-4'>24</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={40} />
+        <span className='text-muted body-4'>40</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={48} />
+        <span className='text-muted body-4'>48</span>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <Spinner size={56} />
+        <span className='text-muted body-4'>56</span>
+      </div>
+    </div>
+  ),
 };
