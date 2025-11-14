c2208e1b7c571281345cef41ab3d4560bbc1711f
Simon Bruneaud <simon.bruneaud@ledger.fr>
2025-11-14 11:52:29 +0100

    fix(rnative): fix Tag and Spot components inheritance + adds app-sandbox-rnative docs
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

---
+++ apps/app-sandbox-rnative/src/app/blocks/Tags.tsx
@@ -0,0 +1,23 @@
+import { Tag } from '@ledgerhq/ldls-ui-rnative';
+import { ExternalLink } from '@ledgerhq/ldls-ui-rnative/symbols';
+import { View } from 'react-native';
+
+export const Tags = () => {
+  return (
+    <View className='gap-32'>
+      <View className='flex flex-row flex-wrap gap-8'>
+        <Tag appearance='accent' label='Accent' icon={ExternalLink} />
+        <Tag appearance='base' label='Base' icon={ExternalLink} />
+        <Tag appearance='gray' label='Gray' icon={ExternalLink} />
+        <Tag appearance='success' label='Success' icon={ExternalLink} />
+        <Tag appearance='error' label='Error' icon={ExternalLink} />
+        <Tag appearance='warning' label='Warning' icon={ExternalLink} />
+        <Tag appearance='disabled' label='Disabled' icon={ExternalLink} />
+      </View>
+      <View className='flex flex-row gap-8'>
+        <Tag appearance='accent' label='Accent' size='lg' />
+        <Tag appearance='accent' label='Accent' size='sm' />
+      </View>
+    </View>
+  );
+};
