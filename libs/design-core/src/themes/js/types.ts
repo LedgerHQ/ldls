ddbf5fb8da8991b4079e359b036d97393b2c8c66
Simon Bruneaud <simon.bruneaud@ledger.fr>
2025-11-25 15:53:18 +0100

    feat(design-core): add primitive constants
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

---
+++ libs/design-core/src/themes/js/types.ts
@@ -0,0 +1,65 @@
+export type PrimitiveTokens = {
+  /**
+   * Colors
+   */
+  colors: {
+    light: {
+      constant: Record<string, string>;
+      grey: Record<string, string>;
+      orange: Record<string, string>;
+      purple: Record<string, string>;
+      blue: Record<string, string>;
+      green: Record<string, string>;
+      yellow: Record<string, string>;
+      red: Record<string, string>;
+      crypto: Record<string, string>;
+      discover: Record<string, string>;
+    };
+    dark: {
+      constant: Record<string, string>;
+      grey: Record<string, string>;
+      purple: Record<string, string>;
+      orange: Record<string, string>;
+      blue: Record<string, string>;
+      green: Record<string, string>;
+      yellow: Record<string, string>;
+      red: Record<string, string>;
+      crypto: Record<string, string>;
+      discover: Record<string, string>;
+    };
+  };
+
+  /**
+   * Sizes & Spacing
+   */
+  spacing: Record<string, number>;
+  size: Record<string, number>;
+  icon: {
+    width: Record<string, number>;
+    height: Record<string, number>;
+    borderWidth: Record<string, number>;
+  };
+  spot: {
+    width: Record<string, number>;
+    height: Record<string, number>;
+  };
+  spinner: {
+    borderWidth: Record<string, number>;
+  };
+
+  /**
+   * Font
+   */
+  fontFamily: Record<string, string>;
+  fontWeight: Record<string, string>;
+  fontSize: Record<string, number>;
+  fontLineHeight: Record<string, number>;
+  fontLetterSpacing: Record<string, number>;
+
+  /**
+   * Others
+   */
+  borderRadius: Record<string, number>;
+  stroke: Record<string, number>;
+  blur: Record<string, number>;
+};
