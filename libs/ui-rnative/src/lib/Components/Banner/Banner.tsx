740446a06e09694662e6a1c8eb336198739e0351
aammami-ledger <abla.ammami@ledger.fr>
2025-11-27 17:10:59 +0100

    feat(ui-rnative): make title optional in Banner component and update stories

--- libs/ui-rnative/src/lib/Components/Banner/Banner.tsx
+++ libs/ui-rnative/src/lib/Components/Banner/Banner.tsx
@@ -99,11 +99,13 @@ export const Banner = React.forwardRef<ViewRef, BannerProps>(
         <View className='flex shrink-0 flex-row items-start py-4'>{icon}</View>
         <View className='mr-8 flex flex-1 flex-col gap-8 py-4'>
           <View className='flex flex-col gap-4'>
-            <Text className='line-clamp-2 text-base body-1-semi-bold'>
-              {title}
-            </Text>
+            {title && (
+              <Text className='line-clamp-2 text-base body-1-semi-bold'>
+                {title}
+              </Text>
+            )}
             {description && (
-              <View className='line-clamp-5 body-2'>
+              <View className={cn('line-clamp-5 body-2', !title && 'mt-2')}>
                 <Wrap
                   if={isTextChildren(description)}
                   with={(children) => (
