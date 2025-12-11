4849285d69aaa35dee0dce844d2fe378f9701595
Simon Bruneaud <simon.bruneaud@ledger.fr>
2025-12-11 18:25:52 +0100

    feat(ui-rnative): add possibility of memoization on the useCreate API
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- libs/ui-rnative/src/styles/create/useCreate.ts
+++ libs/ui-rnative/src/styles/create/useCreate.ts
@@ -1,14 +1,17 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useTheme } from '../Provider/useTheme';
 import type { LumenStyleSheet, LumenStyleSheetExtended } from '../types';
 import { create } from './create';
 
 export const useCreate = <S extends LumenStyleSheet>(
   styleCreator: LumenStyleSheetExtended<S>,
+  deps: any[] = [],
 ): S => {
   const { theme } = useTheme();
+  const styleCreatorMemo = useCallback(styleCreator as Function, deps);
+
 
   return useMemo(() => {
     return create(theme, styleCreator);
-  }, [theme, styleCreator]);
+  }, [theme, styleCreatorMemo]);
 };
--- libs/ui-rnative/src/styles/index.ts
+++ libs/ui-rnative/src/styles/index.ts
@@ -1,6 +1,5 @@
 export { LumenStyleSheetProvider } from './Provider/LumenStyleSheetProvider';
 export { LumenStyleSheet } from './LumenStyleSheet';
 export * from './types';
-export * from './factories/createStyledText';
-export * from './factories/createStyledView';
+export * from './factories';
 export * from './utils/styleVarianceAuthority';
