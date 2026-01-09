4717cb5bc0f3cad8deab19af1ba0db89dc0e0592
Simon.b <15988848+gamegee@users.noreply.github.com>
2026-01-09 12:27:24 +0100

    chore/expose i18n language for easier setup (#403)
    * feat(ui-rnative): expose i18n Languages constants + add docs
    
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>
    
    * feat(ui-react): expose i18n Languages constants + add docs
    
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>
    
    * nx plan
    
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>
    
    ---------
    
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- libs/ui-react/src/i18n/index.ts
+++ libs/ui-react/src/i18n/index.ts
@@ -1,3 +1,3 @@
-export { type SupportedLocale } from './languages';
+export { type SupportedLocale, Languages } from './languages';
 export { I18nProvider } from './I18nProvider';
 export { useCommonTranslation } from './useCommonTranslation';
