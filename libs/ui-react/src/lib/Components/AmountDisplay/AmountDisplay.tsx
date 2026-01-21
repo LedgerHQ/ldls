77828d3397af1b3e8cbd451008e3423e7245049a
Miguel Mano <aquelemiguel@gmail.com>
2026-01-21 17:21:10 +0000

    docs: docs for types

--- libs/ui-react/src/lib/Components/AmountDisplay/AmountDisplay.tsx
+++ libs/ui-react/src/lib/Components/AmountDisplay/AmountDisplay.tsx
@@ -11,14 +11,15 @@ export const AmountDisplay = ({
   return (
     <div className={className}>
       <span className='heading-1-semi-bold text-base' {...props}>
-        {parts.currencyPosition === 'start' && (
-          <span className='mr-4'>{parts.currencyText}</span>
-        )}
+        {parts.currencyPosition === undefined ||
+          (parts.currencyPosition === 'start' && (
+            <span className='mr-4'>{parts.currencyText}</span>
+          ))}
         <span>{parts.integerPart}</span>
       </span>
       <span className='heading-2-semi-bold text-muted'>
-        {parts.decimalPart && parts.decimalSeparator && (
-          <span>{parts.decimalSeparator + parts.decimalPart}</span>
+        {parts.decimalPart && (
+          <span>{(parts.decimalSeparator || '.') + parts.decimalPart}</span>
         )}
         {parts.currencyPosition === 'end' && (
           <span className='ml-4'>{parts.currencyText}</span>
